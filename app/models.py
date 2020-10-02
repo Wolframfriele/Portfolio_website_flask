6a9a736929da4b7343df985a1f5892d30b8d8677
Wolfram Friele <wolframfriele@gmail.com>
2020-10-02 20:02:59 +0200

    Make projects database driven

--- app/models.py
+++ app/models.py
@@ -2,9 +2,34 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, BooleanField, SubmitField, validators
 from wtforms.validators import DataRequired
 from wtforms.fields.html5 import EmailField
+from datetime import date
+from app import db
 
-class ContactForm(FlaskForm):
-    name = StringField('Name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[DataRequired(), validators.Email()])
-    message = StringField('Message', validators=[DataRequired()])
-    submit = SubmitField('Submit')
+# class ContactForm(FlaskForm):
+#     name = StringField('Name', validators=[DataRequired()])
+#     email = EmailField('Email', validators=[DataRequired(), validators.Email()])
+#     message = StringField('Message', validators=[DataRequired()])
+#     submit = SubmitField('Submit')
+
+class Project(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64))
+    short_description = db.Column(db.Text)
+    main_image = db.Column(db.String(255))
+    secondary_image = db.Column(db.String(255))
+    date = db.Column(db.Date)
+    long_description = db.Column(db.Text)
+    video = db.Column(db.Text)
+    project_section = db.relationship('ProjectSection', backref='main_project_info', lazy='dynamic')
+    
+    def __repr__(self):
+        return '<Project {}' .format(self.name)
+
+class ProjectSection(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
+    order = db.Column(db.Integer)
+    html = db.Column(db.Text)
+
+    def __repr__(self):
+        return '<ProjectSection {}' .format(self.id)
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,13 @@
 from flask import render_template, flash, url_for
 from app import app
+from app.models import Project, ProjectSection
 
 @app.route('/')
 @app.route('/index')
 @app.route('/work')
 def index():
-    return render_template('work.html', title='Work')
+    projects = Project.query.all()
+    return render_template('work.html', title='Work', projects=projects)
 
 @app.route('/about')
 def about():
