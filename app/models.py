b0124a54305b1d5e7e39a8ef0cdccf8f68e5b11f
Wolfram Friele <wolframfriele@gmail.com>
2020-10-06 22:38:03 +0200

    Make About and contact mostly dynamic
    Everything comes from a database now except some of the images and some very few used headers.

--- app/models.py
+++ app/models.py
@@ -5,11 +5,22 @@ from wtforms.fields.html5 import EmailField
 from datetime import date
 from app import db
 
-# class ContactForm(FlaskForm):
-#     name = StringField('Name', validators=[DataRequired()])
-#     email = EmailField('Email', validators=[DataRequired(), validators.Email()])
-#     message = StringField('Message', validators=[DataRequired()])
-#     submit = SubmitField('Submit')
+
+#Setting up table for Semi-Static elements
+class StaticElements(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    headline_work = db.Column(db.String(255))
+    subline_work = db.Column(db.Text)
+    cover_image_work = db.Column(db.String(64))
+    headline_about = db.Column(db.String(255))
+    subline_about = db.Column(db.Text)
+    paragraph_about = db.Column(db.Text)
+    headline_contact = db.Column(db.String(255))
+    subline_contact = db.Column(db.Text)
+    hidden_subline_contact = db.Column(db.Text)
+    image_contact = db.Column(db.String(64))
+    headline_experiments = db.Column(db.String(255))
+    subline_experiments = db.Column(db.Text)
 
 # Setting up the table for project entries
 class Project(db.Model):
@@ -32,7 +43,40 @@ class ProjectSection(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'))
     order = db.Column(db.Integer)
-    html = db.Column(db.Text)
+    paragraph = db.Column(db.Text)
+    title = db.Column(db.String(64))
+    image_1 = db.Column(db.String(255))
+    image_2 = db.Column(db.String(255))
+    image_3 = db.Column(db.String(255))
 
     def __repr__(self):
         return '<ProjectSection {}' .format(self.id)
+
+#Setting up table for CV entries
+class CvEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.Integer)
+    start = db.Column(db.Date)
+    end = db.Column(db.Date)
+    workplace = db.Column(db.String(255))
+    paragraph = db.Column(db.Text)
+
+#Setting up table for Course entries
+class CourseEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.Integer)
+    course_name = db.Column(db.String(255))
+    paragraph = db.Column(db.Text)
+
+#Setting up tables for Social Media Links
+class SocialMediaLink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order = db.Column(db.Integer)
+    link = db.Column(db.String(255))
+    display = db.Column(db.String(255))
+
+# class ContactForm(FlaskForm):
+#     name = StringField('Name', validators=[DataRequired()])
+#     email = EmailField('Email', validators=[DataRequired(), validators.Email()])
+#     message = StringField('Message', validators=[DataRequired()])
+#     submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,23 +1,33 @@
 from flask import render_template, flash, url_for
 from app import app
-from app.models import Project, ProjectSection
+from app.models import StaticElements, Project, ProjectSection, CvEntry, CourseEntry, SocialMediaLink
 
 @app.route('/')
 @app.route('/index')
 @app.route('/work')
 def index():
+    static = StaticElements.query.first()
     projects = Project.query.all()[::-1] #Requests the projects fom the database and orders them recent to older
-    return render_template('work.html', title='Work', projects=projects)
+    socials = SocialMediaLink.query.order_by(SocialMediaLink.order).all()
+    return render_template('work.html', title='Work', projects=projects, static=static, socials=socials)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About')
+    static = StaticElements.query.first()
+    cvs = CvEntry.query.order_by(CvEntry.order).all()[::-1]
+    courses = CourseEntry.query.order_by(CourseEntry.order).all()[::-1]
+    socials = SocialMediaLink.query.order_by(SocialMediaLink.order).all()
+    return render_template('about.html', title='About', static=static, cvs=cvs, courses=courses, socials=socials)
 
 @app.route('/contact')
 def contact():
-    return render_template('contact.html', title='Contact')
+    static = StaticElements.query.first()
+    socials = SocialMediaLink.query.order_by(SocialMediaLink.order).all()
+    return render_template('contact.html', title='Contact', static=static, socials=socials)
 
 @app.route('/project/<url_name>')
 def project(url_name):
     project = Project.query.filter_by(url_name=url_name).first_or_404()
-    return render_template('project.html', title=project.name, project=project)
+    sections = ProjectSection.query.order_by(ProjectSection.order).filter_by(project_id=project.id)
+    socials = SocialMediaLink.query.order_by(SocialMediaLink.order).all()
+    return render_template('project.html', title=project.name, project=project, sections=sections, socials=socials)
